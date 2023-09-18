4c8211689be7dd6f194b3f2821dfee153883443f
Ram Sharma <ram@exiliensoft.com>
2023-09-18 17:44:19 +0530

    replicate-app-commit-for-page-mono-repo

--- apps/jest.config.ts
+++ apps/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../coverage/page-mono-repo-app',
+  coverageDirectory: '../coverage/apps',
 };
